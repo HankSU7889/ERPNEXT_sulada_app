4c85d967c2e61a9913631cdb390b4dc3db80334a
Hanksu <hongkaisu@hotmail.com>
2024-07-21 14:23:58 +0800

    Initial commit

---
+++ sulada_app/sulada_app/doctype/in_and_out_data/in_and_out_data.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, SuHongKai and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("In and out data", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ sulada_app/sulada_app/doctype/in_and_out_data/in_and_out_data.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, SuHongKai and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Inandoutdata(Document):
+	pass
